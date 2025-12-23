4f9a3e1a14784214d7d2e5cb9351e4ab360d3eb2
zpolonius <zacharias@polonius.dk>
2025-12-23 19:37:31 +0100

    added host permissions in vite.config.ts and CORS settings in main.py

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Vi tillader de lokale domæner, vi bruger til test
+    allowedHosts: [
+      'returnwiz.local',
+      'app.returnwiz.local',
+      'myshop.returnwiz.local'
+    ]
+  }
 })
